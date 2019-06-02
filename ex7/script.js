0394d722cdf55c6e1d211ab9e005ea4fa1cdaa9a
Alan Klein <aklein@osisecure.com>
2019-06-02 08:37:31 -0400

    Add Bonus Ex.7 Smooth Slide Transition

--- ex7/script.js
+++ ex7/script.js
@@ -2,14 +2,14 @@
 // BONUS: Can you make the section slide up and down like this? :
 // https://www.w3schools.com/bootstrap/bootstrap_collapse.asp
 
-var sectionHdr = document.getElementById("section-header");
-console.log(sectionHdr);
+// var sectionHdr = document.getElementById('section-header');
+// console.log(sectionHdr);
 
-sectionHdr.addEventListener("click", function() {
-  let section = document.getElementById("section");
-  if (section.getAttribute("style") === "display: none;") {
-    section.style.display = "block";
-  } else {
-    section.style.display = "none";
-  }
-});
+// sectionHdr.addEventListener('click', function() {
+//   // let section = document.getElementById("section");
+//   // if (section.getAttribute("style") === "display: none;") {
+//   //   section.style.display = "block";
+//   // } else {
+//   //   section.style.display = "none";
+//   // }
+// });
